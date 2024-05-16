5839b58e1463b9976f45aa9ac0ebe0299addb07b
Shanjida Nahid Shorna <sanjidanahid53@gmail.com>
2024-05-16 23:55:29 +0600

    first commit

---
+++ js/active.js
@@ -0,0 +1,56 @@
+$(document).ready(function(){
+    $('.slider-active').owlCarousel({
+       
+        loop:true,
+        nav:true,
+        navText:[ ' <i class="fa-solid fa-angle-left"></i>','<i class="fa-solid fa-angle-right"></i>'],
+
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    })
+    $('.project-active').owlCarousel({
+       
+        loop:true,
+        nav:true,
+        navText:[ ' <i class="fa-solid fa-angle-left"></i>','<i class="fa-solid fa-angle-right"></i>'],
+
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:4
+            },
+            1000:{
+                items:4
+            }
+        }
+    })
+    $('.blog-active').owlCarousel({
+       
+        loop:true,
+        nav:true,
+        navText:[ ' <i class="fa-solid fa-angle-left"></i>','<i class="fa-solid fa-angle-right"></i>'],
+
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    })
+})
